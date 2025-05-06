e8a84364d25df31395ce169a9c8b4d660d9a62ee
someone <nabthedev@gmail.com>
2025-05-06 13:24:10 +0200

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userId INTEGER,
+    content TEXT,
+    FOREIGN KEY (userId) REFERENCES users (id)
+);
